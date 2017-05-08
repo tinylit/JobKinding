8d97efbacfe4bf068c159918f55e02d2f4bf041b
KimiCriss <jschar@foxmail.com>
2017-05-08 17:35:27 +0800

    Add files via upload

---
+++ api/jobkinding.dialog.js
@@ -0,0 +1,21 @@
+﻿$.api.init("Dialog", {
+    init: function () {
+        this.base.init("div");
+        this.$.after('<div class="layer"></div>');
+    },
+    resolve: function () {
+        this.base.resolve();
+        this.setHeader(this.header);
+        this.setFooter(this.footer);
+        this.setContaiter(this.contaiter);
+    },
+    setHeader: function (options) {
+        this.T = this.dynamic(options, ".ui.header");
+    },
+    setContaiter: function (options) {
+        this.Y = this.dynamic(options || {}, ".ui.contaiter");
+    },
+    setFooter: function (options) {
+        this.V = this.dynamic(options, ".ui.footer");
+    }
+});
