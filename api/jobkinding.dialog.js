2f9200c8951dd7c75810b14f244e4dd77c4b82cf
KimiCriss <jschar@foxmail.com>
2017-05-11 16:17:43 +0800

    Add files via upload

--- api/jobkinding.dialog.js
+++ api/jobkinding.dialog.js
@@ -1,21 +1,23 @@
-﻿$.api.init("Dialog", {
-    init: function () {
-        this.base.init("div");
-        this.$.after('<div class="layer"></div>');
-    },
-    resolve: function () {
-        this.base.resolve();
-        this.setHeader(this.header);
-        this.setFooter(this.footer);
-        this.setContaiter(this.contaiter);
-    },
-    setHeader: function (options) {
-        this.T = this.dynamic(options, ".ui.header");
-    },
-    setContaiter: function (options) {
-        this.Y = this.dynamic(options || {}, ".ui.contaiter");
-    },
-    setFooter: function (options) {
-        this.V = this.dynamic(options, ".ui.footer");
-    }
-});
+﻿(function ($) {
+    $.api.init("Dialog", {
+        init: function () {
+            this.base.init("div");
+            this.$.after('<div class="layer"></div>');
+        },
+        resolve: function () {
+            this.base.resolve();
+            this.setHeader(this.header);
+            this.setFooter(this.footer);
+            this.setContaiter(this.contaiter);
+        },
+        setHeader: function (options) {
+            this.T = this.dynamic(options, ".ui.header");
+        },
+        setContaiter: function (options) {
+            this.Y = this.dynamic(options || {}, ".ui.contaiter");
+        },
+        setFooter: function (options) {
+            this.V = this.dynamic(options, ".ui.footer");
+        }
+    });
+})(jobKinding);
---
+++ api/jobkinding.taskbar.js
@@ -0,0 +1,49 @@
+﻿(function ($) {
+    $.api.init("Taskbar", {
+        init: function () {
+            this.base.init("div");
+        },
+        render: function () {
+            this.task = [];
+            this.base.render();
+            this.$.addClass("ui menu transition");
+        },
+        resolve: function () {
+            this.base.resolve();
+            this.dynamic({
+                html2on: {
+                    "i.fa-angle-double-up": function (that) {
+                        that.show();
+                    },
+                    "i.fa-angle-double-down": function (that) {
+                        that.hide();
+                    }
+                }
+            }, this.$);
+        },
+        addTask: function (taskName, taskCallbak) {
+            if (arguments.length === 1 && typeof taskName === "function") {
+                taskCallbak = taskName;
+                taskName = null;
+            }
+            var task = {
+                name: taskName || "任务",
+                click: taskCallbak
+            };
+            var that = this;
+            task.$ = this.$.append('<div class="item">' + task.name + "</div>").lastChild();
+            if (taskCallbak) {
+                task.$.click(this.contextCall(taskCallbak));
+            }
+            this.task.push(task);
+            return task;
+        },
+        removeTask: function (task) {
+            var index = this.task.indexOf(task);
+            if (index > -1) {
+                this.task.splice(index, 1);
+            }
+            return index > -1;
+        }
+    });
+})(jobKinding);
