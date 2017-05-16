aa5eeb99d3ef75e6828e1e4c6495b36ceb1ebd0d
KimiCriss <jschar@foxmail.com>
2017-05-16 17:33:04 +0800

    Add files via upload

--- api/jobkinding.dialog.js
+++ api/jobkinding.dialog.js
@@ -8,20 +8,15 @@
         },
         render: function () {
             this.base.render();
+            if (this.visible) {
+                this.show();
+            }
+            if (this.isMax) {
+                this.max();
+            }
             if (this.transition && $.api.animation) {
                 this.$.addClass(this.transition);
             }
-        },
-        resolve: function () {
-            this.base.resolve();
-            this.setHeader(this.header);
-            this.setFooter(this.footer);
-            this.setContaiter(this.contaiter);
-        },
-        show: function () {
-            this.base.show();
-            this.layer.css("z-index", $.api.zIndex++);
-            this.$.css("z-index", $.api.zIndex++);
         },
         setHeader: function (options) {
             this.T = this.dynamic(options, ".ui.header");
@@ -31,6 +26,47 @@
         },
         setFooter: function (options) {
             this.V = this.dynamic(options, ".ui.footer");
+        },
+        resolve: function () {
+            this.base.resolve();
+            this.setHeader(this.header);
+            this.setFooter(this.footer);
+            this.setContaiter(this.contaiter);
+        },
+        show: function () {
+            var that = this;
+            this.base.show();
+            this.layer.css("z-index", $.api.zIndex++);
+            this.$.css("z-index", $.api.zIndex++);
+            setTimeout(function () {
+                that.$.addClass("transition-visible");
+            }, 200)
+        },
+        hide: function () {
+            this.base.hide();
+            this.$.removeClass("transition-visible");
+        },
+        toggleScreen: function (v) {
+            if (typeof v === "boolean") {
+                return v ? this.max() : this.restore();
+            }
+            return this.isMax ? this.restore() : this.max();
+        },
+        max: function () {
+            this.isMax = true;
+            this.$.css({ height: "100%", width: "100%" });
+        },
+        restore: function () {
+            this.isMax = false;
+            this.setWidth(this.width);
+            this.setHeight(this.height);
+        },
+        close: function (isClose) {
+            if (isClose || this.isClose) {
+                this.destroy();
+            } else {
+                this.hide();
+            }
         }
     });
 })(jobKinding);
